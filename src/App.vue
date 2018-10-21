4ea5eace90dce82ef766b0f1687a54864de3c447
Jenya Trofimov <deadrime@yandex.ru>
2018-10-21 15:25:44 +0300

    Change font

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Exo 2', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
